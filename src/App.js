304b227cc9e44cc35ab1e3ba35ed50dd3103d16e
lordziaga <lordziaga@gmail.com>
2023-11-20 15:17:42 +0700

    New Project Movie Landing Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const search = (q) => {
+    console.log({q})
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ansal Movie</h1>
+        <div className="Movie-container">
+          <input placeholder="cari movie ..." className="Movie-search"
+          onChange={({target}) => search(target.value)}
+          />
+          
+          <div className="Movie-wrapper">
+            <div className="Movie-title">judul film</div>
+            <img className="Movie-image" src=" "/>
+            <div className="Movie-date">11-11-2023</div>
+            <div className="Movie-rate">8.7</div>
+          </div>
+        </div>
       </header>
     </div>
   );
