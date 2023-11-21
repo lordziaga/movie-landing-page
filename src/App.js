9749f7bf968c95f8865d9b572626765eb329480b
lordziaga <lordziaga@gmail.com>
2023-11-21 12:19:40 +0700

    fect data API "Failled" error

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import './App.css';
-import {getMovieList, searchMovie} from "./api"
-import {useEffect} from "react"
+import { getMovieList, searchMovie } from "./api"
+import { useEffect, useState } from "react"
+
 const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
   useEffect(() => {
-    getMovieList()
-  })
-  const search = (q) => {
-    console.log({q})
+      getMovieList().then((result) => {
+        setPopularMovies(result)
+      })
+    }, [])
+        
+  const PopularMovieList = () => {
+    return popularMovies.map((movie) => (
+      <div className="Movie-wrapper" key={movie.id}>
+        <div className="Movie-title">{movie.title}</div>
+        <img className="Movie-image" src={movie.poster_path} alt={movie.title} />
+        <div className="Movie-date">{movie.release_date}</div>
+        <div className="Movie-rate">{movie.vote_average}</div>
+      </div>
+    ));
   }
+
+  const search = (query) => {
+    console.log({ query });
+    // Add logic to perform movie search and update state accordingly
+  }
+
+  // console.log({ popularMovies: popularMovies });
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ansal Movie</h1>
+        <input
+          placeholder="cari movie ..."
+          className="Movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
         <div className="Movie-container">
-          <input placeholder="cari movie ..." className="Movie-search"
-          onChange={({target}) => search(target.value)}
-          />
-          
-          <div className="Movie-wrapper">
-            <div className="Movie-title">judul film</div>
-            <img className="Movie-image" src=" "/>
-            <div className="Movie-date">11-11-2023</div>
-            <div className="Movie-rate">8.7</div>
-          </div>
+          <PopularMovieList />
         </div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,9 +1,31 @@
 import axios from "axios"
 
-const getMovieList =async() => {
-    const movie = await axios.get(`${process.env.REACT_APP_BASEURL}/movie/popular`)
+const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/popular',
+    params: {language: 'en-US', page: '1'},
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTkyNDA4M2FhMjA0MDk4MmQ0MzUwMjc3ZTM4YjBkOSIsInN1YiI6IjY1NWIwMzJiNjdkY2M5MDBjNjg4MjNiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wjr1AAg7phvOjIWsTz2BUNXLdYAAb5CK5KdTfGV555U'
+    }
+  };
+  axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+  
+const apiKey = process.env.REACT_APP_APIKEY
+const baseUrl = process.env.REACT_APP_BASEURL
+
+export const getMovieList = async () => {
+    const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`)
+    // return movie.data.results
     console.log({movieList: movie})
-}
+    }
 
 export const searchMovie = async (q) => {
     const search = await axios.get(q)
